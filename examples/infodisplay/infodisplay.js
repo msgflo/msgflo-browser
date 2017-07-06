2071d750699282e438dd798666fe41e79a41d966
Henri Bergius <henri.bergius@iki.fi>
2017-07-06 11:56:19 +0200

    Initial infodisplay example

---
+++ examples/infodisplay/infodisplay.js
@@ -0,0 +1,48 @@
+var msgflo = window.msgflo;
+var DisplayParticipant = function (broker, role) {
+  var element = document.getElementById('iframe');
+  var def = {
+    component: 'msgflo-browser/infodisplay',
+    inports: [
+      {
+        id: 'show',
+        type: 'string'
+      }
+    ],
+    outports: [
+      {
+        id: 'showed',
+        type: 'string'
+      }
+    ]
+  };
+  var process = function (inport, indata, callback) {
+    console.log(inport, indata);
+    element.setAttribute('src', indata);
+    return callback('showed', null, indata);
+  };
+  var client = new msgflo.mqtt.Client(broker, {});
+  return new msgflo.participant.Participant(client, def, process, role);
+}
+
+window.addEventListener('load', function () {
+  var params = {
+    broker: 'mqtt://localhost',
+    role: 'infodisplay'
+  }
+  if (window.location.search) {
+    window.location.search.substr(1).split('&').forEach(function (p) {
+      var split = p.split('=');
+      params[split[0]] = decodeURIComponent(split[1]);
+    });
+  }
+  console.log(params);
+  var p = DisplayParticipant(params.broker, params.role);
+  p.start(function (err) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Started');
+  });
+}, false);
