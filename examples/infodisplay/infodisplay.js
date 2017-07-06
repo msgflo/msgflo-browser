8a991cb3c10365fa6489e4b11dd5b79f0a97a3ae
Henri Bergius <henri.bergius@iki.fi>
2017-07-06 16:25:42 +0200

    Implement internal URL rotation

--- examples/infodisplay/infodisplay.js
+++ examples/infodisplay/infodisplay.js
@@ -1,4 +1,18 @@
 var msgflo = window.msgflo;
+var urls = [
+    "http://c-beam.cbrp3.c-base.org/events",
+    "https://c-beam.cbrp3.c-base.org/c-base-map",
+    "https://c-beam.cbrp3.c-base.org/weather",
+    "http://c-beam.cbrp3.c-base.org/bvg",
+    "http://c-beam.cbrp3.c-base.org/nerdctrl",
+    "https://c-beam.cbrp3.c-base.org/sensors",
+    "https://c-beam.cbrp3.c-base.org/ceitloch",
+    "http://app.flowhub.io#runtime/endpoint?protocol%3Dwebsocket%26address%3Dws%3A%2F%2Fc-flo.cbrp3.c-base.org%3A3569%26id%3Da9dca883-c07f-4cd7-b369-180fa9b52b68",
+];
+var getRotationUrl = function () {
+  return urls[Math.floor(Math.random() * urls.length)];
+};
+
 var DisplayParticipant = function (broker, role) {
   var element = document.getElementById('iframe');
   var def = {
@@ -23,9 +37,14 @@ var DisplayParticipant = function (broker, role) {
       return callback('showed', null, element.getAttribute('src'));
     };
     element.setAttribute('src', indata);
+    // Rotate internal URLs list
+    setTimeout(function () {
+      participant.send('show', getRotationUrl());
+    }, 120000);
   };
   var client = new msgflo.mqtt.Client(broker, {});
-  return new msgflo.participant.Participant(client, def, process, role);
+  var participant = new msgflo.participant.Participant(client, def, process, role);
+  return participant;
 }
 
 window.addEventListener('load', function () {
@@ -40,5 +59,6 @@ window.addEventListener('load', function () {
       return;
     }
     console.log('Started');
+    p.send('show', getRotationUrl());
   });
 }, false);
