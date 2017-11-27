0f0acba6733f6ed5f884347c4ec11e0f67325d6f
Henri Bergius <henri.bergius@iki.fi>
2017-11-27 20:11:15 +0100

    Run tests with headless chrome

--- spec/utils/phantom.js
+++
@@ -1,7 +0,0 @@
-var webPage = require('webpage');
-var page = webPage.create();
-
-page.open('http://localhost:3000/examples/infodisplay/index.html?msgflo_broker=mqtt://localhost&msgflo_role=display', function(status) {
-  console.log('Status: ' + status);
-  // Do other things here...
-});
